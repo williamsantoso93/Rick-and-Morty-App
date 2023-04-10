10aded82314bf1605ac3c73e40698c6d669db976
William Santoso <williamsantoso@keda-tech.com>
2023-04-10 21:15:55 +0700

    initial commit

---
+++ Rick-and-Morty-App/Rick_and_Morty_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Rick_and_Morty_AppApp.swift
+//  Rick-and-Morty-App
+//
+//  Created by William Santoso on 10/04/23.
+//
+
+import SwiftUI
+
+@main
+struct Rick_and_Morty_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
