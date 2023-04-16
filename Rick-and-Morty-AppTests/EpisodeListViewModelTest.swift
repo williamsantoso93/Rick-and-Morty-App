cb884e632fc8ecd737e267d41d2890eae1dad6bb
William Santoso <williamsantoso@keda-tech.com>
2023-04-16 11:37:57 +0700

    update test location and episode

--- Rick-and-Morty-AppTests/EpisodeListViewModelTest.swift
+++ Rick-and-Morty-AppTests/EpisodeListViewModelTest.swift
@@ -26,7 +26,13 @@ final class EpisodeListViewModelTest: XCTestCase {
     }
     
     func test_EpisodeList_GivenMoreData_ShouldGetListSucceccfully() async {
-        await viewModel.fetchList()
+        let list = [
+            Episode(id: 19, name: "Interdimensional Cable 2: Tempting Fate", airDate: "September 20, 2015", episodeSeason: "S02E08", characters: [], url: "https://rickandmortyapi.com/api/episode/19", created: "2017-11-10T12:56:35.669Z"),
+            Episode(id: 20, name: "Look Who\'s Purging Now", airDate: "September 27, 2015", episodeSeason: "S02E09", characters: [], url: "https://rickandmortyapi.com/api/episode/20", created: "2017-11-10T12:56:35.772Z")
+        ]
+        viewModel.setList(list)
+        
+        let nextUrl = "https://rickandmortyapi.com/api/episode?page=2"
         
         await getMoreData()
         
@@ -37,7 +43,7 @@ final class EpisodeListViewModelTest: XCTestCase {
     }
     
     func test_EpisodeListSearch_GivenName_ShouldGetListSucceccfully() async {
-        viewModel.searchText = "pilot"
+        viewModel.searchText = "rick"
         await viewModel.fetchList()
         
         DispatchQueue.main.async {
@@ -46,8 +52,14 @@ final class EpisodeListViewModelTest: XCTestCase {
     }
     
     func test_EpisodeListSearch_GivenNameMoreData_ShouldGetListSucceccfully() async {
-        viewModel.searchText = "pilot"
-        await viewModel.fetchList()
+        let list = [
+            Episode(id: 41, name: "Star Mort: Rickturn of the Jerri", airDate: "May 31, 2020", episodeSeason: "S04E10", characters: [], url: "https://rickandmortyapi.com/api/episode/41", created: "2020-08-06T05:51:52.079Z"),
+            Episode(id: 42, name: "Mort Dinner Rick Andre", airDate: "June 20, 2021", episodeSeason: "S05E01", characters: [], url: "https://rickandmortyapi.com/api/episode/42", created: "2021-10-15T17:00:24.099Z")
+        ]
+        viewModel.setList(list)
+        
+        let nextUrl = "https://rickandmortyapi.com/api/episode?page=2&name=Rick"
+        viewModel.setNextUrl(nextUrl)
         
         await getMoreData()
         
